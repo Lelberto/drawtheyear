77581ca4c081910fd884713b85109adfee34c683
Lelberto <jsurieux@gmail.com>
2023-05-05 20:59:17 +0200

    Add Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('DrawTheYear API')
+    .setVersion('1.0')
+    .build();
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDoc);
+
   app.enableCors(); // TODO Configure CORS
+  
   await app.listen(80);
 }
+
 bootstrap();
