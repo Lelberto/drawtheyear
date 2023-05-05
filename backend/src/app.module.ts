176be8c4a1481272e9fc5599a0237d65f6dcf46d
Lelberto <jsurieux@gmail.com>
2023-05-05 19:53:55 +0200

    Update App module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigurationModule } from './config/configuration.module';
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './models/users/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigurationModule,
+    DatabaseModule,
+    AuthModule,
+    UserModule
+  ]
 })
 export class AppModule {}
