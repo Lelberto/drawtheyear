7628f91857f296d091090800b9d7fc0b73f705f5
Lelberto <jsurieux@gmail.com>
2023-05-06 18:06:34 +0200

    Add environment in startup scripts Add environment logging at application startup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigurationModule } from './config/configuration.module';
 import { DatabaseModule } from './database/database.module';
@@ -12,4 +12,11 @@ import { UserModule } from './models/users/user.module';
     UserModule
   ]
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+
+  private readonly logger = new Logger(AppModule.name);
+
+  public onModuleInit() {
+    this.logger.log(`Environment : ${process.env.NODE_ENV}`);
+  }
+}
