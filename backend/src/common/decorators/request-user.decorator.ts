3874909c35cf5f6741e972c3200d7ab7e0a1385a
Lelberto <jsurieux@gmail.com>
2023-05-06 19:18:07 +0200

    Update user request decorator

--- backend/src/common/decorators/request-user.decorator.ts
+++ backend/src/common/decorators/request-user.decorator.ts
@@ -1,7 +1,9 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { Request } from 'express';
+import { User } from '../../models/users/entities/user.entity';
 
-export const ReqUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+export const ReqUser = createParamDecorator((data: keyof User, ctx: ExecutionContext) => {
   const req = ctx.switchToHttp().getRequest<Request>();
-  return req.user;
+  const user = req.user as User;
+  return data ? user?.[data] : user;
 });
