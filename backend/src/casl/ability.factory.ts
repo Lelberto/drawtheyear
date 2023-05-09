ecb30c0a55fcca9d9b63dd73abb4f2a1066bbbeb
Lelberto <jsurieux@gmail.com>
2023-05-09 23:10:35 +0200

    Add CASL module

---
+++ backend/src/casl/ability.factory.ts
@@ -0,0 +1,29 @@
+import { AbilityTuple, ExtractSubjectType, MongoAbility, MongoQuery, Subject, defineAbility } from '@casl/ability';
+import { Injectable } from '@nestjs/common';
+import { Emotion } from '../models/emotions/entities/emotion.entity';
+import { User } from '../models/users/entities/user.entity';
+import { Action } from './action.enum';
+import { Role } from '../common/constants/role.enum';
+
+type AppAbility = MongoAbility<AbilityTuple, MongoQuery>;
+
+@Injectable()
+export class AbilityFactory {
+
+  public createForUser(user: User): AppAbility {
+    return defineAbility(can => {
+      switch (user.role) {
+        case Role.ADMIN:
+          can(Action.MANAGE, 'all');
+          break;
+        case Role.USER:
+          can(Action.READ, Emotion);
+          can(Action.CREATE, Emotion);
+          can(Action.UPDATE, Emotion, { userId: user.id });
+          break;
+      }
+    }, {
+      detectSubjectType: subject => subject.constructor as ExtractSubjectType<Subject>
+    });
+  }
+}
